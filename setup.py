4f173368e828e37d1a96abe571afb13bdbc3fad3
Tamil-selvan10 <etamilselvan2710111996@gmail.com>
2023-10-06 13:52:40 +0530

    latest

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from typing import List
+
+
+
+PROJECT_NAME ='Housing-Predictor'
+VERSION='0.0.1'
+AUTHOR='TAMIL'
+DESCRIPTION='This is a First Machine Learning Project'
+PACKAGES=['housing']
+REQUIREMENT_FILE_NAME='requirements.txt'
+
+def get_requirements_list()->List[str]:
+    '''
+    Description: This function is going to return the name of libraries in the requirements.txt file
+    '''
+    with open (file=REQUIREMENT_FILE_NAME,mode='r') as f:
+        return f.readlines()
+
+setup(
+    name=PROJECT_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    description=DESCRIPTION,
+    packages=PACKAGES,
+    install_requires=get_requirements_list()
+)
+
+if __name__=='__main__':
+    print(get_requirements_list())
